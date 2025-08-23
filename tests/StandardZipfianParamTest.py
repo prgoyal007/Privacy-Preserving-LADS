f5812de67974fa3277a4634c4f07e7192dc9be41
Prabhav Goyal <prabhav.goyal@gmail.com>
2025-08-23 15:35:55 -0700

    Update Project Directory Structure

--- tests/StandardZipfianParamTest.py
+++ tests/StandardZipfianParamTest.py
@@ -22,7 +22,7 @@ Summary:
 
 import json
 
-from DataGenerator import *
+from tests.DataGenerator import *
 from structures.StaticRSL import *
 from structures.BiasedZipZipTree import *
 from structures.ThresholdZipZipTree import *
