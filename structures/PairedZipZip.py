f0245a0872ef1f8a2ea4c16c001f7cf55266226c
Prabhav Goyal <prabhav.goyal@gmail.com>
2025-09-11 12:11:50 -0700

    Update PairedZipZip.py

--- structures/PairedZipZip.py
+++ structures/PairedZipZip.py
@@ -3,7 +3,7 @@ from structures.BiasedZipZipTree import *
 gamma = 1
 
 class PairedZipZipTree:
-	
+
 	"""
 	Initialize a PairedZipZipTree.
 
@@ -11,9 +11,10 @@ class PairedZipZipTree:
 	- capacity : Maximum number of nodes expected (affects rank scaling)
 	"""
 	def __init__(self, capacity: int):
-    	self.biased_zip = BiasedZipZipTree(capacity)
-    	self.standard_zip = BiasedZipZipTree(capacity)
-    	self.limit_walk = gamma
+		self.biased_zip = BiasedZipZipTree(capacity)
+		self.standard_zip = BiasedZipZipTree(capacity)
+		self.limit_walk = gamma
+	
 
 	"""
 	Insert a new key-value pair into the ZipZipTree, optionally using a given rank.
@@ -33,8 +34,8 @@ class PairedZipZipTree:
 	- Increments self.size by 1.
 	"""
 	def insert(self, key: KeyType, val: ValType, freq: float, rank: Rank = None):
-    	self.biased_zip.insert(key, val, freq, rank)
-    	self.standard_zip.insert(key, val, None, rank)
+		self.biased_zip.insert(key, val, freq, rank)
+		self.standard_zip.insert(key, val, None, rank)
 
 
 	"""
@@ -49,9 +50,10 @@ class PairedZipZipTree:
 	- Decrements self.size by 1.
 	"""
 	def remove(self, key: KeyType):
-    	self.biased_zip.remove(key)
-    	self.standard_zip.remove(key)
+		self.biased_zip.remove(key)
+		self.standard_zip.remove(key)
 
+	
 	"""
 	Find the value associated with a key.
 
@@ -63,7 +65,8 @@ class PairedZipZipTree:
 	- Value associated with the key if found, else None.
 	"""
 	def find(self, key: KeyType):
-    	return self.standard_zip.find()	
+		return self.standard_zip.find()
+	
 
 	"""
 	Find a value by key and track the number of comparisons performed.
@@ -77,15 +80,18 @@ class PairedZipZipTree:
 		- value : value associated with key (None if not found)
 		- cost : number of comparisons performed
 	"""
-	def find_with_cost(self, key: KeyType): 
-    	n = self.biased_zip.get_size()
-    	found, cost1 = self.biased_zip.find_with_cost(key, self.limit_walk * math.floor(math.log2(n)))
-    	if found is False:
-      		true_found, cost2 = self.standard_zip.find_with_cost(key)
-    	total_cost = cost1 + cost2
+	def find_with_cost(self, key: KeyType):
+		n = self.biased_zip.get_size()
+		cost1, cost2 = 0
+		found, cost1 = self.biased_zip.find_with_cost(key, self.limit_walk * math.floor(math.log2(n)))
 
-    return true_found, total_cost
-    
+		if found is False:
+			true_found, cost2 = self.standard_zip.find_with_cost(key)
+		
+		total_cost = cost1 + cost2
+
+		return true_found, total_cost
+	
 
 	"""
 	Return the current number of nodes in the ZipZipTree.
@@ -95,9 +101,8 @@ class PairedZipZipTree:
 	"""
 	def get_size(self) -> int:
 		return self.biased_zip.get_size() + self.standard_zip.get_size()
-
-
-
+	
+	
 	"""
 	Compute the height of the ZipZipTree.
 
@@ -106,3 +111,4 @@ class PairedZipZipTree:
 	"""
 	def get_height(self) -> int:
 		return self.biased_zip.get_height()
+	
